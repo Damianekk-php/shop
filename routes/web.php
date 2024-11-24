b43961bde33235856e1c708baa1e11a788e4bb7e
Damianekk-php <damian.glab07@gmail.com>
2024-11-24 20:30:15 +0100

    #15 Lista uzytkownikow

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/users/list', [UserController::class, 'index'])->middleware('auth');
 Route::get('/hello', [HelloController::class, 'show']);
 
 
